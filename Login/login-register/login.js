4947e6db028026188736d334aed19f896233bded
Johnny0109-idk <152876363+Johnny0109-idk@users.noreply.github.com>
2026-02-24 21:23:45 +0700

    Cuoi Ki
    Upfile

---
+++ Login/login-register/login.js
@@ -0,0 +1,21 @@
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+  
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+  
+    const matchedUser = users.find(
+      (user) => user.username === username && user.password === password
+    );
+  
+    if (matchedUser) {
+      alert("🎉 Login successful!");
+      window.location.href = "/Homepage/Index.html"; 
+    } else {
+      alert("⚠️ Invalid username or password.");
+    }
+  });
+  
